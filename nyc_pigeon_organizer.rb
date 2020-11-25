f0f296b11fd612b28bdf29f8e44cdecb5094632e
Kody Samaroo <kody.samaroo@gmail.com>
2020-11-25 05:42:18 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,12 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   # write your code here!
-  data.each_with_object({}) do |(k, v)|, output|
+  data.each_with_object({}) do |(key, val)|, output|
+    value.each do |key2, val2|
+      v2.each do |name|
+        hash.New = name
+      end
+    end
     output
   end
   binding.pry
